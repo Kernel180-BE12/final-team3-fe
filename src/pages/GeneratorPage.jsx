51f033888cb7f42ff4cf591960b06613b18d7b3e
steve <hong.git@yahoo.com>
2025-09-18 11:46:38 +0900

    Update GeneratorPage.jsx

--- src/pages/GeneratorPage.jsx
+++ src/pages/GeneratorPage.jsx
@@ -398,7 +398,7 @@ export default function GeneratorPage() {
                 <div className="w-11 h-6 bg-gray-200 rounded-full peer peer-focus:ring-4 peer-focus:ring-blue-300 peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-0.5 after:left-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all peer-checked:bg-indigo-600"></div>
               </label>
               <button className="bg-indigo-600 text-white px-4 py-2 rounded-md hover:bg-indigo-700 text-sm font-medium">
-                이 카톡 승인하기
+                이 템플릿 승인하기
               </button>
             </div>
           </header>
