386615f278419ace9a194de2e2eed1a4622e1b17
steve <hong.git@yahoo.com>
2025-09-22 05:06:52 +0900

    Speed Insights를 추가

--- src/App.jsx
+++ src/App.jsx
@@ -1,42 +1,57 @@
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import LandingPage from './pages/LandingPage';
-import LoginPage from './pages/LoginPage';
-import SignupPage from './pages/SignupPage';
-import DashboardPage from './pages/DashboardPage';
-import GeneratorPage from './pages/GeneratorPage';
-import TemplatesPage from './pages/TemplatesPage';
-import ProtectedRoute from './components/ProtectedRoute';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import LandingPage from "./pages/LandingPage";
+import LoginPage from "./pages/LoginPage";
+import SignupPage from "./pages/SignupPage";
+import DashboardPage from "./pages/DashboardPage";
+import GeneratorPage from "./pages/GeneratorPage";
+import TemplatesPage from "./pages/TemplatesPage";
+import ProtectedRoute from "./components/ProtectedRoute";
+import { SpeedInsights } from "@vercel/speed-insights/react";
+import "./App.css";
 
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<LandingPage />} />
-        <Route path="/login" element={<LoginPage />} />
-        <Route path="/signup" element={<SignupPage />} />
-        
-        {/* 보호된 라우트들 */}
-        <Route path="/dashboard" element={
-          <ProtectedRoute>
-            <DashboardPage />
-          </ProtectedRoute>
-        } />
-        
-        <Route path="/templates" element={
-          <ProtectedRoute>
-            <TemplatesPage />
-          </ProtectedRoute>
-        } />
-        
-        <Route path="/create" element={
-          <ProtectedRoute>
-            <GeneratorPage />
-          </ProtectedRoute>
-        } />
-      </Routes>
-    </Router>
+    <>
+      <Router>
+        <Routes>
+          <Route path="/" element={<LandingPage />} />
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/signup" element={<SignupPage />} />
+
+          {/* 보호된 라우트들 */}
+          <Route
+            path="/dashboard"
+            element={
+              <ProtectedRoute>
+                <DashboardPage />
+              </ProtectedRoute>
+            }
+          />
+
+          <Route
+            path="/templates"
+            element={
+              <ProtectedRoute>
+                <TemplatesPage />
+              </ProtectedRoute>
+            }
+          />
+
+          <Route
+            path="/create"
+            element={
+              <ProtectedRoute>
+                <GeneratorPage />
+              </ProtectedRoute>
+            }
+          />
+        </Routes>
+      </Router>
+
+      {/* Speed Insights를 Router 밖에 배치 */}
+      <SpeedInsights />
+    </>
   );
 }
 
